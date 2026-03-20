e02d0968a13171bc6833fe41dea380476fb48d28
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-20 15:19:28 +0300

    feat(controller): implement GET /welcome and POST /students endpoints with constructor injection

--- src/main/java/org/zenith/springbootstudentapi/controller/StudentController.java
+++ src/main/java/org/zenith/springbootstudentapi/controller/StudentController.java
@@ -1,14 +1,25 @@
 package org.zenith.springbootstudentapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zenith.springbootstudentapi.model.Student;
+import org.zenith.springbootstudentapi.service.StudentService;
+import java.util.List;
 
 @RestController
 public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping("/welcome")
     public String welcome(@RequestParam String name) {
-        return "Welcome " + name;
+        return studentService.welcomeMessage(name);
+    }
+
+    @PostMapping("/students")
+    public String createStudents(@RequestBody List<Student> newStudents) {
+        return studentService.addStudents(newStudents);
     }
 }
