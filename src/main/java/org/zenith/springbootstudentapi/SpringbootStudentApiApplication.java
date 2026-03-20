2b33d6872dacf3b3ec1c586c181b34cf1cd1448f
DavFilsDev <miharisoadavidfils@gmail.com>
2026-03-20 14:07:40 +0300

    feat(init): add default structure by maven

---
+++ src/main/java/org/zenith/springbootstudentapi/SpringbootStudentApiApplication.java
@@ -0,0 +1,13 @@
+package org.zenith.springbootstudentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootStudentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/zenith/springbootstudentapi/SpringbootStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zenith.springbootstudentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
